c24758e9e05edcc0c3ce453307c1fcd0cd56a780
shoaib-khalid <shoaib@kalsym.com>
2024-01-06 17:52:18 +0500

    committing Readme.md file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hello-rust-aws-s3
 A basic example repo for using aws rust sdk for accessing amazon s3 service
 
+Branch: rust-s3-aws
+In this branch rust-s3 crate is used to access aws-s3 service
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use aws_config::{meta::region::RegionProviderChain, Region};
-use aws_config::BehaviorVersion;
-use aws_sdk_s3::{Client, Error};
+// use aws_config::{meta::region::RegionProviderChain, Region};
+// use aws_config::BehaviorVersion;
+// use aws_sdk_s3::{Client, Error};
 
 /// Lists your DynamoDB tables in the default Region or us-east-1 if a default Region isn't set.
 #[tokio::main]
@@ -20,67 +20,67 @@ async fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-async fn show_buckets(strict: bool, client: &Client, region: &str) -> Result<(), Error> {
-    let resp = client.list_buckets().send().await?;
-    let buckets = resp.buckets();
-    let num_buckets = buckets.len();
+// async fn show_buckets(strict: bool, client: &Client, region: &str) -> Result<(), Error> {
+//     let resp = client.list_buckets().send().await?;
+//     let buckets = resp.buckets();
+//     let num_buckets = buckets.len();
 
-    let mut in_region = 0;
+//     let mut in_region = 0;
 
-    for bucket in buckets {
-        if strict {
-            let r = client
-                .get_bucket_location()
-                .bucket(bucket.name().unwrap_or_default())
-                .send()
-                .await?;
+//     for bucket in buckets {
+//         if strict {
+//             let r = client
+//                 .get_bucket_location()
+//                 .bucket(bucket.name().unwrap_or_default())
+//                 .send()
+//                 .await?;
 
-            if r.location_constraint().unwrap().as_ref() == region {
-                println!("{}", bucket.name().unwrap_or_default());
-                in_region += 1;
-            }
-        } else {
-            println!("{}", bucket.name().unwrap_or_default());
-        }
-    }
+//             if r.location_constraint().unwrap().as_ref() == region {
+//                 println!("{}", bucket.name().unwrap_or_default());
+//                 in_region += 1;
+//             }
+//         } else {
+//             println!("{}", bucket.name().unwrap_or_default());
+//         }
+//     }
 
-    println!();
-    if strict {
-        println!(
-            "Found {} buckets in the {} region out of a total of {} buckets.",
-            in_region, region, num_buckets
-        );
-    } else {
-        println!("Found {} buckets in all regions.", num_buckets);
-    }
+//     println!();
+//     if strict {
+//         println!(
+//             "Found {} buckets in the {} region out of a total of {} buckets.",
+//             in_region, region, num_buckets
+//         );
+//     } else {
+//         println!("Found {} buckets in all regions.", num_buckets);
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
 
-async fn upload_file(client:&Client) -> Result<(), std::io::Error> {
-    //let shared_config = aws_config::load_from_env().await;
-    //let client = S3Client::new(&shared_config);
+// async fn upload_file(client:&Client) -> Result<(), std::io::Error> {
+//     //let shared_config = aws_config::load_from_env().await;
+//     //let client = S3Client::new(&shared_config);
 
-    let bucket_name = "asimbucket1".to_string(); // Use your existing bucket
-    let region_provider = RegionProviderChain::first_try(Region::new("us-east-1")); // Use the "us-east-1" region
-    let region = region_provider.region().await.unwrap();
+//     let bucket_name = "asimbucket1".to_string(); // Use your existing bucket
+//     let region_provider = RegionProviderChain::first_try(Region::new("us-east-1")); // Use the "us-east-1" region
+//     let region = region_provider.region().await.unwrap();
 
-    let key = "D:\\shoaib_file_aws.txt".to_string(); // Replace with the path to your file
+//     let key = "D:\\shoaib_file_aws.txt".to_string(); // Replace with the path to your file
 
-    // Read the file
-    let data = tokio::fs::read(&key).await?;
+//     // Read the file
+//     let data = tokio::fs::read(&key).await?;
 
-    // Upload the file
-    let _resp = client.put_object()
-        .bucket(&bucket_name)
-        .key(&key)
-        .body(data.into())
-        .send()
-        .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+//     // Upload the file
+//     let _resp = client.put_object()
+//         .bucket(&bucket_name)
+//         .key(&key)
+//         .body(data.into())
+//         .send()
+//         .await
+//         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
 
-    println!("File uploaded successfully");
+//     println!("File uploaded successfully");
 
-    Ok(())
+//     Ok(())
 
-}
+// }
